076cfb36fbc5421740af7b13a6c5b4241d60917f
zhongjp <1225930063@qq.com>
2020-06-29 23:29:48 +0800

    BeanPostProcessor和BeanFactoryPostProcessor的区别

---
+++ spring-test-demo/src/main/java/com/zhongjp/springTest/CustomBeanFactoryPostProcessor.java
@@ -0,0 +1,13 @@
+package com.zhongjp.springTest;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+		User user = (User) beanFactory.getBean("user");
+		System.out.println(user.toString());
+	}
+}
---
+++ spring-test-demo/src/main/java/com/zhongjp/springTest/MyBeanPostProcessor.java
@@ -0,0 +1,19 @@
+package com.zhongjp.springTest;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("第五步：BeanPostProcessor，对象" + beanName + "调用初始化方法之前的数据： " + bean.toString());
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("第八步：BeanPostProcessor，对象" + beanName + "调用初始化方法之后的数据：" + bean.toString());
+		return bean;
+	}
+}
